83a49af8c2b1b5c8c7c67a353d0074037b8181cb
Arthur Biagi <arthurvalsezia@gmail.com>
2026-08-31 22:38:42 -0300

    feat: criação do main e fuções

--- poo/01-fundamentos-java/3108/Pessoa.java
+++ poo/01-fundamentos-java/3108/Pessoa.java
@@ -1,3 +1,21 @@
 public class Pessoa {
-    
+    static int cpf = 0;
+    static String nome = "";
+
+    public static void main(String arg[]){
+        int a = 7;
+        String b = "El Artu";
+
+        entDados(a, b);
+        impDados();
+    }
+
+    public static void entDados(int c, String n){
+        cpf = c;
+        nome = n;
+    }
+
+    public static void impDados(){
+        System.out.println();
+    }
 }
