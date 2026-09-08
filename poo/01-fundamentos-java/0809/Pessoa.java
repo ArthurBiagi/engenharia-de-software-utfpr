9100526fdcb6de43b9bd670bc5aec0e31f15c285
Arthur Biagi <arthurvalsezia@gmail.com>
2026-09-08 20:40:35 -0300

    feat: contrução classe pessoa

--- poo/01-fundamentos-java/0809/Pessoa.java
+++ poo/01-fundamentos-java/0809/Pessoa.java
@@ -3,15 +3,6 @@ public class Pessoa{
     int cpf = 0;
     String nome = "";
 
-    public static void main(String arg[]){
-        Pessoa p1 = new Pessoa();
-        p1.setCpf(133);
-        p1.setNome("Kadoah");
-
-        System.out.println("\n CPF...: "+p1.getCpf());
-        System.out.println("\n NOME...: "+p1.getNome());
-
-    }
 
     // Getters --> recuperam os valores dos atributos
     public int getCpf(){
