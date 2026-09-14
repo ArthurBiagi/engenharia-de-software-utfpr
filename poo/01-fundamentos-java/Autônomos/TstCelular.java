3bc729d03bdde2a519c02a13d65915f19d2a225e
Arthur Biagi <arthurvalsezia@gmail.com>
2026-09-14 18:48:33 -0300

    docs: Criação do main do objeto

---
+++ poo/01-fundamentos-java/Autônomos/TstCelular.java
@@ -0,0 +1,12 @@
+public class TstCelular {
+
+    public static void main(String arg[]) {
+        Celular c1 = new Celular(); // declaração e instanciação
+
+        // Usando o método de entrada de dados
+        c1.entDados("Samsung", 2023);
+
+        // Usando o método de impressão de dados
+        c1.impDados();
+    }
+}
